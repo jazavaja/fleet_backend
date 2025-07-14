f0dab416ca08545c3ee02f812b702ce6fba44ba6
Javad <abtinafraz@gmail.com>
2025-07-14 14:14:29 +0430

    Navymain,NavyBrand,NavySize,NavyTpye perfectly Works OK

--- fleets/urls.py
+++ fleets/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import NavyTypeViewSet, NavySizeViewSet, NavyBrandViewSet, NavyMainViewSet
+from .views import NavyTypeViewSet, NavySizeViewSet, NavyBrandViewSet, NavyMainViewSet, get_sizes_by_type, \
+    get_brands_by_size
 
 router = DefaultRouter()
 router.register(r'types', NavyTypeViewSet, basename='navy-type')
@@ -12,4 +13,6 @@ router.register(r'main', NavyMainViewSet, basename='navy-main')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('sizes/by-type/<int:type_id>/', get_sizes_by_type, name='sizes-by-type'),
+    path('brands/by-size/<int:size_id>/', get_brands_by_size, name='brands-by-size')
 ]
--- fleets/views.py
+++ fleets/views.py
@@ -3,6 +3,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 from fleets.models import NavyType, NavyBrand, NavyMain, NavySize
 from fleets.serializers import NavySizeSerializer, NavyTypeSerializer, NavyMainSerializer, NavyBrandSerializer
@@ -47,3 +50,29 @@ class NavyMainViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['name']
     permission_classes = [IsAuthenticated]
+
+
+
+
+
+@api_view(['GET'])
+def get_sizes_by_type(request, type_id):
+    try:
+        type_instance = NavyType.objects.get(id=type_id)
+        sizes = type_instance.sizes.all()  # related_name in NavySize.types
+        serializer = NavySizeSerializer(sizes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except NavyType.DoesNotExist:
+        return Response({'error': 'NavyType not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def get_brands_by_size(request, size_id):
+    try:
+        size_instance = NavySize.objects.get(id=size_id)
+        brands = size_instance.brands.all()  # related_name in NavyBrand.sizes
+        serializer = NavyBrandSerializer(brands, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except NavySize.DoesNotExist:
+        return Response({'error': 'NavySize not found'}, status=status.HTTP_404_NOT_FOUND)
+
